98fb1b92d1c0c3c7ded18e3c0614cdabdb1715ca
trungld3 <k21tpm4@gmail.com>
2020-05-19 11:19:53 +0700

    touchbegan and return button controlling the keyboard

--- Controling the Keyboard/ViewController.swift
+++ Controling the Keyboard/ViewController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
 }
 
